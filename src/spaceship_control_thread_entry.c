f378737812beb00558022a263b44e39f8c98fb09
gabriellinke <gh.linke@hotmail.com>
2023-06-16 20:01:34 -0300

    Adiciona interrupção do botão

--- src/spaceship_control_thread_entry.c
+++ src/spaceship_control_thread_entry.c
@@ -1,11 +1,29 @@
+#include "hal_data.h"
+#include <stdio.h>
 #include "spaceship_control_thread.h"
+extern void initialise_monitor_handles(void);
 
 /* Spaceship Control Thread entry function */
 void spaceship_control_thread_entry(void)
 {
-    /* TODO: add your own code here */
+    initialise_monitor_handles(); //Used for printf outputs
+    /* Initialize button interruption */
+    button_interrupt.p_api->open(button_interrupt.p_ctrl,button_interrupt.p_cfg); // Open instance and initialize.
+    ULONG message;
+
     while (1)
     {
-        tx_thread_sleep (1);
+        UINT status = tx_queue_receive(&button_queue, &message, TX_WAIT_FOREVER);
+        if(!status) {
+            printf("\nBotão apertado\n");
+        } else {
+            printf("Error: %d", status);
+        }
+
     }
+}
+
+void button_callback(external_irq_callback_args_t *p_args) {
+    ULONG parameter_message = 1;
+    UINT status = tx_queue_send(&button_queue, &parameter_message, TX_NO_WAIT);
 }
--- src/synergy_gen/hal_data.c
+++ src/synergy_gen/hal_data.c
@@ -1,5 +1,24 @@
 /* generated HAL source file - do not edit */
 #include "hal_data.h"
+#if (5) != BSP_IRQ_DISABLED
+#if !defined(SSP_SUPPRESS_ISR_button_interrupt) && !defined(SSP_SUPPRESS_ISR_ICU11)
+SSP_VECTOR_DEFINE( icu_irq_isr, ICU, IRQ11);
+#endif
+#endif
+static icu_instance_ctrl_t button_interrupt_ctrl;
+static const external_irq_cfg_t button_interrupt_cfg =
+{ .channel = 11,
+  .trigger = EXTERNAL_IRQ_TRIG_RISING,
+  .filter_enable = false,
+  .pclk_div = EXTERNAL_IRQ_PCLK_DIV_BY_64,
+  .autostart = true,
+  .p_callback = button_callback,
+  .p_context = &button_interrupt,
+  .p_extend = NULL,
+  .irq_ipl = (5), };
+/* Instance structure to use this module. */
+const external_irq_instance_t button_interrupt =
+{ .p_ctrl = &button_interrupt_ctrl, .p_cfg = &button_interrupt_cfg, .p_api = &g_external_irq_on_icu };
 void g_hal_init(void)
 {
     g_common_init ();
